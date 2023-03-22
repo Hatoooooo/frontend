20c5ef3c3a11ce1d92b7fab2bcb6b701222e08e2
Hatoooooo <alvesanthony20@gmail.com>
2023-03-22 11:43:39 +0100

    Ajout de la naviguation / Refactoring composant

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,18 @@
 import '../styles/FooterStyle.css'
-function Footer() {
-    return ( 
-        <footer className="footer">
-            <div>
-                <h4>Oui</h4>
-            </div>
-            <div>
-                <h4>Non</h4>
-            </div>
-            <div>
-                <h4>Bien-sur</h4>
-            </div>
-        </footer>
-     );
+function Footer () {
+  return (
+    <footer className='footer'>
+      <div>
+        <h4>Oui</h4>
+      </div>
+      <div>
+        <h4>Non</h4>
+      </div>
+      <div>
+        <h4>Bien-sur</h4>
+      </div>
+    </footer>
+  )
 }
 
-export default Footer;
+export default Footer
---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+function About () {
+  return (
+    <h1>A PROPOS</h1>
+
+  )
+}
+
+export default About
